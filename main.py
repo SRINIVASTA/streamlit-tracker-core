e2894f2284038e335a4082c8d4dd54018a5cb9a1
T A Srinivas <tasrinivass@gmail.com>
2026-07-22 19:36:40 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from tracker import log_viewer_activity
-
-# Automatically runs background checks and database routing
-log_viewer_activity("Dashboard_App_Name_Here")
-
-# --- Continue with your original app logic ---
-st.title("Welcome to my App")
